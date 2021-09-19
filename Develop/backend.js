6f5bfc7c0396fb42f6179c70232ad1e19328d3b3
Asante Cureton <asantercureton@gmail.com>
2021-09-19 12:27:11 -0400

    updated backend.js file

--- Develop/backend.js
+++ Develop/backend.js
@@ -9,6 +9,7 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
 
 // GET * should return the index.html file.
 app.get('*', (req, res) =>
@@ -22,18 +23,59 @@ app.get('/notes', (req, res) =>
 
 const readFromFile = util.promisify(fs.readFile);
 
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+ const writeToFile = (destination, content) =>
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+ );
+
+/**
+*  Function to read data from a given a file and append some content
+*  @param {object} content The content you want to append to the file.
+*  @param {string} file The path to the file you want to save to.
+*  @returns {void} Nothing
+*/
+const readAndAppend = (content, file) => {
+ fs.readFile(file, 'utf8', (err, data) => {
+   if (err) {
+     console.error(err);
+   } else {
+     const parsedData = JSON.parse(data);
+     parsedData.push(content);
+     writeToFile(file, parsedData);
+   }
+ });
+};
+
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-  return res.json(jsonDB);
-  console.info(`${req.method} request received`);
+  console.info(`${req.method} request received for saved notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received`);
 
-  res.json(`${req.method} request received`);
-  console.info(req.rawHeaders);
+  const { title, note } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
 
 app.listen(PORT, () =>
