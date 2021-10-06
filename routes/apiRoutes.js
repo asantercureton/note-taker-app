bb4bf139849e03199e317b003f738cb69ccf4c60
Asante Cureton <asantercureton@gmail.com>
2021-10-06 09:01:55 -0400

    fixed bug for delete functionality and re-recorded video for up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,63 +2,66 @@ const router = require('express').Router();
 const uuid = require('../helpers/uuid');
 const fs = require('fs');
 const util = require('util');
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-const notes = require('../db/db.json');
-const notesSaved = notes && notes.length ? notes : [];
+const allNotes = require('../db/db.json');
+const path = require('path');
+
+const notesSaved = allNotes && allNotes.length ? allNotes : [];
 
 // GET ALL NOTES
 router.get('/notes', (req, res) => {
-  res.json(notesSaved);
+    res.json(notesSaved);
 })
 
-// SAVE NOTES
-router.post('/notes', (req, res) => {  
+// SAVE NOTE
+router.post('/notes', (req, res) => {
     const { title, text } = req.body;
-  
+
     if (title && text) {
-      const newNote = {
-        id: uuid(),
-        title,
-        text,
-      };
-  
-      notesSaved.push(newNote);
-  
-      const storedNote = JSON.stringify(notesSaved, null, 2);
-  
-      fs.writeFile('./db/db.json', storedNote, (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `Note for ${newNote.title} has been written to JSON file`
-            )
-      );
-  
-      const response = {
-        status: 'Successful',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
+        const newNote = {
+            id: uuid(),
+            title,
+            text,
+        };
+        // PUSH NEW NOTE INTO SAVED NOTES ARRAY
+        notesSaved.push(newNote);
+        const storedNote = JSON.stringify(notesSaved, null, 2);
+        // WRITE TO DB.JSON FILE
+        fs.writeFile('./db/db.json', storedNote, (err) =>
+            err
+                ? console.error(err)
+                : console.log(
+                    `Successful save!`
+                )
+        );
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        res.status(201).json(response);
     } else {
-      res.status(500).json('Error with saving note');
+        res.status(500).json('There was an error saving note');
     }
-  });
+});
 
-  router.delete('/notes/:id', (req, res) => {
-    notesSaved.splice(req.params.id, 1);
-    const storedNote = JSON.stringify(notesSaved, null, 2)
-    
-    fs.writeFile('./db/db.json', storedNote, (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `Note has been deleted from JSON file`
-            ));
+// DELETE NOTE
+const deleteNote = (id, notesArr) => {
+    for (let i = 0; i < notesArr.length; i++) {
+        let note = notesArr[i];
+        if (note.id == id) {
+            notesArr.splice(i, 1);
+            // WRITE TO DB.JSON FILE
+            fs.writeFileSync('./db/db.json', JSON.stringify(notesArr, null, 2)
+            );
 
-    return res.json(notesSaved);
-  })
+            break;
+        };
+    };
+};
 
-  module.exports = router;
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+module.exports = router;
